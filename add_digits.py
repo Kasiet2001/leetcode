d2d2a70f4aa5a9c03fd023638c852bf62c104a66
Kasiet <mursidinovakasiet@gmail.com>
2023-04-26 19:49:25 +0600

    new solved leetcode problem

--- add_digits.py
+++ add_digits.py
@@ -1,5 +1,11 @@
 def addDigits(num):
-    while num > 9:
-        num = sum([int(i) for i in str(num)])
-    return num
+    #while num > 9:
+        #num = sum([int(i) for i in str(num)])
+    #return num
+    if num == 0:
+        return 0
+    elif num % 9 == 0:
+        return 9
+    else:
+        return num % 9
 print(addDigits(38))
