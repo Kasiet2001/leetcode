7697a965aea9e0ef91f8768d96a4eeb7b7dde32a
Kasiet <mursidinovakasiet@gmail.com>
2023-05-10 18:21:45 +0600

    new solved leetcode problem

--- spiral_matrix_II.py
+++ spiral_matrix_II.py
@@ -1,26 +1,24 @@
 def generateMatrix(n):
-    if not n:
-        return []
-    matrix = [[0 for _ in range(n)] for _ in range(n)]
-    t, b, l, r, num = 0, n - 1, 0, n - 1, 1
-    while l <= r and t <= b:
-        for i in range(l, r + 1):
-            matrix[t][i] = num
+    matrix = [[0] * n for _ in range(n)]
+    left, right = 0, n - 1
+    top, bottom = 0, n - 1
+    num = 1
+    while left <= right:
+        for i in range(left, right + 1):
+            matrix[top][i] = num
             num += 1
-        t += 1
-        for i in range(t, b + 1):
-            matrix[i][r] = num
+        top += 1
+        for i in range(top, bottom + 1):
+            matrix[i][right] = num
             num += 1
-        r -= 1
-        if t <= b:
-            for i in range(r, l - 1, -1):
-                matrix[b][i] = num
-                num += 1
-            b -= 1
-        if l <= r:
-            for i in range(b, t - 1, -1):
-                matrix[i][l] = num
-                num += 1
-            l += 1
+        right -= 1
+        for i in range(right, left - 1, -1):
+            matrix[bottom][i] = num
+            num += 1
+        bottom -= 1
+        for i in range(bottom, top - 1, -1):
+            matrix[i][left] = num
+            num += 1
+        left += 1
     return matrix
 print(generateMatrix(3))
