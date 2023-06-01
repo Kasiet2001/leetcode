71a4a1a1d95569d15947839ff7ce1c839ae6385a
Kasiet Mursidinova <kasietmursidinova@Kasiets-MacBook-Pro.local>
2023-06-01 17:47:19 +0600

    new solved leetcode problem

--- longest_palindrome_by_concatenating_two_letter_words.py
+++ longest_palindrome_by_concatenating_two_letter_words.py
@@ -1,4 +1,12 @@
 from collections import defaultdict
 def longestPalindrome(words):
-
+    ans = 0
+    d = defaultdict(int)
+    for i in words:
+        if d[i] > 0:
+            ans += 4
+            d[i] -= 1
+        else:
+            d[i[::-1]] += 1
+    return ans + (2 if any(k[0] == k[1] for k, v in d.items() if v == 1) else 0)
 print(longestPalindrome(["mm","mm","yb","by","bb","bm","ym","mb","yb","by","mb","mb","bb","yb","by","bb","yb","my","mb","ym"]))
