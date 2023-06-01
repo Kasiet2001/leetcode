ef417fddd489fb51103bc7148cb31abe962c5153
Kasiet Mursidinova <kasietmursidinova@Kasiets-MacBook-Pro.local>
2023-06-01 17:46:52 +0600

    new solved leetcode problem

---
+++ longest_palindrome_by_concatenating_two_letter_words.py
@@ -0,0 +1,4 @@
+from collections import defaultdict
+def longestPalindrome(words):
+
+print(longestPalindrome(["mm","mm","yb","by","bb","bm","ym","mb","yb","by","mb","mb","bb","yb","by","bb","yb","my","mb","ym"]))
