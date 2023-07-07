6a75f963380c24f0e3a0e186ae2f17e988cceac0
Kasiet Mursidinova <kasietmursidinova@Kasiets-MacBook-Pro.local>
2023-07-07 13:04:31 +0600

    new solved leetcode problem

---
+++ maximize_the_confusion_of_an_exam.py
@@ -0,0 +1,11 @@
+def maxConsecutiveAnswers(answerKey, k):
+    max_freq = i = 0
+    d = {'T': 0, 'F': 0}
+    for j in range(len(answerKey)):
+        d[answerKey[j]] += 1
+        max_freq = max(max_freq, d[answerKey[j]])
+        if j - i + 1 > max_freq + k:
+            d[answerKey[i]] -= 1
+            i += 1
+    return len(answerKey) - i
+print(maxConsecutiveAnswers("TTFF", 2))
