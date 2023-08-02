ee0300e5f3f79066a5e26148453687336b74cdba
Kasiet Mursidinova <kasietmursidinova@Kasiets-MacBook-Pro.local>
2023-08-02 16:50:18 +0600

    new solved leetcode problem

--- happy_number.py
+++ happy_number.py
@@ -1,8 +1,13 @@
 def isHappy(n):
     end_num = 0
+    d = dict()
     while len(str(n ** 2)) != 1:
         for i in str(n):
             end_num += int(i)**2
+        if end_num in d:
+            return False
+        else:
+            d[end_num] = True
         end_num, n = 0, end_num
     return n == 1
-print(isHappy(19))
+print(isHappy(4))
