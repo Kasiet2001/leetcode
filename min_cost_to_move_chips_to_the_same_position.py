ad63bd113429248aae9690b57cbc89c7effa9084
Kasiet Mursidinova <kasietmursidinova@Kasiets-MacBook-Pro.local>
2023-09-15 19:03:48 +0600

    new solved leetcode problem

---
+++ min_cost_to_move_chips_to_the_same_position.py
@@ -0,0 +1,10 @@
+def minCostToMoveChips(position):
+    even = 0
+    odd = 0
+    for i in position:
+        if i % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+    return min(odd, even)
+print(minCostToMoveChips())
