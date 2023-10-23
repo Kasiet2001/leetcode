584edb0a5011011ac0b27e605d61ef6160698550
Kasiet Mursidinova <kasietmursidinova@Kasiets-MacBook-Pro.local>
2023-10-23 14:10:57 +0600

    new solved leetcode problem

---
+++ min_sum_of_mountain_triplets_II.py
@@ -0,0 +1,13 @@
+def minimumSum(nums):
+    pref_min = [nums[0]] * len(nums)
+    suf_min = [nums[-1]] * len(nums)
+    for i in range(1, len(nums)):
+        pref_min[i] = min(pref_min[i - 1], nums[i])
+    for i in range(len(nums) - 2, -1, -1):
+        suf_min[i] = min(suf_min[i + 1], nums[i])
+    ans = float('inf')
+    for i in range(1, len(nums) - 1):
+        if pref_min[i - 1] < nums[i] and suf_min[i + 1] < nums[i]:
+            ans = min(ans, pref_min[i - 1] + nums[i] + suf_min[i + 1])
+    return -1 if ans == float('inf') else ans
+print(minimumSum([8,6,1,5,3]))
--- power_of_four.py
+++ power_of_four.py
@@ -1,4 +1,12 @@
 def isPowerOfFour(n):
-    from math import log
-    return n > 0 and log(n, 3).is_integer()
+    if n == 1:
+        return True
+    if n <= 0:
+        return False
+
+    while n > 1:
+        if n % 4 != 0:
+            return False
+        n //= 4
+    return True
 print(isPowerOfFour(243))
