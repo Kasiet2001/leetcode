804984a63f5d4c16114ac5abd7e88bee0a8b053e
Kasiet Mursidinova <kasietmursidinova@Kasiets-MacBook-Pro.local>
2023-10-31 19:05:10 +0600

    new solved leetcode problem

--- min_increment_operations_to_make_array_beautiful.py
+++ min_increment_operations_to_make_array_beautiful.py
@@ -1,7 +1,7 @@
 def minIncrementOperations(nums, k):
-    dp = [0] * 3
+    dp = [0] * len(nums)
     dp[0], dp[1], dp[2] = max(0, k - nums[0]), max(0, k - nums[1]), max(0, k - nums[2])
     for i in range(3, len(nums)):
-        dp.append(min(dp[i - 1], dp[i - 2], dp[i - 3]) + max(0, k - nums[i]))
-    return min(dp[len(dp) - 3:])
+        dp[i] = min(dp[i - 1], dp[i - 2], dp[i - 3]) + max(0, k - nums[i])
+    return min(dp[-1], dp[2], dp[-3])
 print(minIncrementOperations([2,3,0,0,2], 4))
