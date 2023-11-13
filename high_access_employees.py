9a38467bb801471d4bb6873e099b408a6eecc70e
Kasiet Mursidinova <kasietmursidinova@Kasiets-MacBook-Pro.local>
2023-11-13 14:49:19 +0600

    new solved leetcode problem

---
+++ high_access_employees.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+def findHighAccessEmployees(access_times):
+    d = defaultdict(list)
+    for e, t in access_times:
+        d[e].append(int(t[:2]) * 60 + int(t[2:]))
+    ans = []
+    for k, v in d.items():
+        if len(v) > 2:
+            v.sort()
+            for i in range(2, len(v)):
+                if v[i] - v[i - 2] < 60:
+                    if k not in ans:
+                        ans.append(k)
+    return ans
+print(findHighAccessEmployees([["cd","1025"],["ab","1025"],["cd","1046"],["cd","1055"],["ab","1124"],["ab","1120"]]))
