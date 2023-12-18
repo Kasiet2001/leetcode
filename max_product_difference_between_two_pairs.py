121f6ce6ced3e188513b2f4b1ba08c25b7d42db1
Kasiet Mursidinova <kasietmursidinova@Kasiets-MacBook-Pro.local>
2023-12-18 14:03:01 +0600

    new solved leetcode problem

---
+++ max_product_difference_between_two_pairs.py
@@ -0,0 +1,14 @@
+def maxProductDifference(nums):
+    first_big = second_big = 0
+    first_small = second_small = float('inf')
+    for n in nums:
+        if n < first_small:
+            second_small, first_small = first_small, n
+        elif n < second_small:
+            second_small = n
+        if n > first_big:
+            second_big, first_big = first_big, n
+        elif n > second_big:
+            second_big = n
+    return first_big * second_big - first_small * second_small
+print(maxProductDifference([5,6,2,7,4]))
