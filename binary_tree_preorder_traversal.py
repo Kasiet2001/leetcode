852b8d4bab33b87bd57acf7c8b0a805c95dad09e
Kasiet Mursidinova <kasietmursidinova@Kasiets-MacBook-Pro.local>
2024-01-27 18:51:28 +0600

    new solved leetcode problem

---
+++ binary_tree_preorder_traversal.py
@@ -0,0 +1,12 @@
+def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    ans = []
+
+    def preorder(root):
+        if not root:
+            return None
+        ans.append(root.val)
+        preorder(root.left)
+        preorder(root.right)
+
+    preorder(root)
+    return ans
