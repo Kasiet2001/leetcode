964976817a79d6a701ffa5e34b64c2a052d72e1e
Kasiet Mursidinova <kasietmursidinova@Kasiets-MacBook-Pro.local>
2024-03-14 06:32:54 +0600

    new solved leetcode problem

---
+++ insert_greatest_common_divisors_in_linked_list.py
@@ -0,0 +1,12 @@
+from fractions import gcd
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def insertGreatestCommonDivisors(self, head):
+        curr = head
+        while curr and curr.next:
+            curr.next = ListNode(gcd(curr.val, curr.next.val), curr.next)
+            curr = curr.next.next
+        return head
