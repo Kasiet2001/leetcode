761db8f31cd309410123226b1ec83e5823a27caa
Kasiet Mursidinova <kasietmursidinova@Kasiets-MacBook-Pro.local>
2024-04-25 17:45:47 +0600

    new solved leetcode problem

---
+++ longest_ideal_subsequence.py
@@ -0,0 +1,11 @@
+def longestIdealString(s, k):
+    dp = [0] * 26
+    for c in s:
+        curr = ord(c) - ord('a')
+        longest = 1
+        for prev in range(26):
+            if abs(curr - prev) <= k:
+                longest = max(longest, 1 + dp[prev])
+        dp[curr] = max(dp[curr], longest)
+    return max(dp)
+print(longestIdealString("acfgbd", 2))
