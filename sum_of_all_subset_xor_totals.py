2f4f2a3c84d0403112d3ebfb157bf4c57ee93fd6
Kasiet Mursidinova <kasietmursidinova@Kasiets-MacBook-Pro.local>
2024-05-20 16:14:23 +0600

    new solved leetcode problem

---
+++ sum_of_all_subset_xor_totals.py
@@ -0,0 +1,11 @@
+def subsetXORSum(nums):
+    n = len(nums)
+    total_sum = 0
+    for i in range(1 << n):
+        subset_xor = 0
+        for j in range(n):
+            if i & (1 << j):
+                subset_xor ^= nums[j]
+        total_sum += subset_xor
+    return total_sum
+print(subsetXORSum([3,4,5,6,7,8]))
