823743c09e4dc907c7dfc2595d05fd31583dee9d
Kasiet Mursidinova <kasietmursidinova@Kasiets-MacBook-Pro.local>
2024-06-07 17:26:42 +0600

    new solved leetcode problem

---
+++ find_champion_II.py
@@ -0,0 +1,8 @@
+def findChampion(n, edges):
+    e = {i: 1 for i in range(n)}
+    for s, w in edges:
+        if w in e:
+            e.pop(w)
+    ans = list(e.keys())
+    return ans[0] if len(ans) == 1 else -1
+print(findChampion(1, []))
