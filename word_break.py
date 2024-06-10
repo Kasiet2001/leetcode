c13788aff4d55682261a9d4599f87fd31d4a6b40
Kasiet Mursidinova <kasietmursidinova@Kasiets-MacBook-Pro.local>
2024-06-10 17:46:50 +0600

    new solved leetcode problem

---
+++ word_break.py
@@ -0,0 +1,11 @@
+def wordBreak(s, wordDict):
+    dp = [False] * (len(s) + 1)
+    dp[len(s)] = True
+    for i in range(len(s) - 1, -1, -1):
+        for w in wordDict:
+            if i + len(w) <= len(s) and s[i: i + len(w)] == w:
+                dp[i] = dp[i + len(w)]
+            if dp[i]:
+                break
+    return dp[0]
+print(wordBreak("catsandog", ["cats","dog","sand","and","cat"]))
