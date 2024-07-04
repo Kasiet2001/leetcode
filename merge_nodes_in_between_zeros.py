a81b80f1d8c1f6b38e8f1af70525d4501100fca9
Kasiet Mursidinova <kasietmursidinova@Kasiets-MacBook-Pro.local>
2024-07-04 13:16:20 +0600

    new solved leetcode problem

---
+++ merge_nodes_in_between_zeros.py
@@ -0,0 +1,18 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeNodes(self, head):
+        modify = head.next
+        next_sum = modify
+        while next_sum:
+            sum = 0
+            while next_sum.val != 0:
+                sum += next_sum.val
+                next_sum = next_sum.next
+            modify.val = sum
+            next_sum = next_sum.next
+            modify.next = next_sum
+            modify = modify.next
+        return head.next
