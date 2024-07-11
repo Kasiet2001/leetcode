4f836dbec89aa4df3647c21ed32629de0fad925c
Kasiet2001 <mursidinovakasiet@gmail.com>
2024-07-11 16:32:24 +0600

    new solved leetcode problem

--- reverse_substring_between_each_pair_of_parentheses.py
+++ reverse_substring_between_each_pair_of_parentheses.py
@@ -4,20 +4,19 @@ def reverseParentheses(s):
     for ch in s:
         if ch.isalpha():
             curr_str += ch
-        elif ch == '(':
-            if curr_str:
-                stack.append(curr_str)
-                curr_str = ''
-            stack.append(ch)
         else:
             stack.append(curr_str)
             curr_str = ''
-            while stack and stack[-1] != '(':
-                curr_str = stack.pop() + curr_str
-            stack.pop()
-            stack.append(curr_str[::-1])
-            curr_str = ''
-
+            if ch == ')':
+                stack.append(curr_str)
+                curr_str = ''
+                while stack and stack[-1] != '(':
+                    curr_str = stack.pop() + curr_str
+                stack.pop()
+                stack.append(curr_str[::-1])
+                curr_str = ''
+            else:
+                stack.append(ch)
     return ''.join(stack) + curr_str if curr_str else ''.join(stack)
 print(reverseParentheses("a(bcdefghijkl(mno)p)q"))
 
