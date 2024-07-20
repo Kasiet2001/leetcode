4858031d24e419b8283ae6c99af411b578283998
Kasiet2001 <mursidinovakasiet@gmail.com>
2024-07-20 15:19:29 +0600

    new solved leetcode problem

---
+++ find_valid_matrix_given_row_and_column_sums.py
@@ -0,0 +1,20 @@
+def restoreMatrix(rowSum, colSum):
+    N = len(rowSum)
+    M = len(colSum)
+
+    orig_matrix = [[0] * M for _ in range(N)]
+    i, j = 0, 0
+
+    while i < N and j < M:
+        orig_matrix[i][j] = min(rowSum[i], colSum[j])
+
+        rowSum[i] -= orig_matrix[i][j]
+        colSum[j] -= orig_matrix[i][j]
+
+        if rowSum[i] == 0:
+            i += 1
+        else:
+            j += 1
+
+    return orig_matrix
+print(restoreMatrix([3,8], [4,7]))
