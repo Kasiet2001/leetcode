b9d93480f620c1ecf1fa04e7847520912219cf01
Kasiet2001 <mursidinovakasiet@gmail.com>
2024-09-13 14:58:06 +0600

    new solved leetcode problem

---
+++ xor_queries_of_a_subarray.py
@@ -0,0 +1,7 @@
+def xorQueries(arr, queries):
+    prefix_xor = [0] * (len(arr) + 1)
+    for i in range(len(arr)):
+        prefix_xor[i + 1] = prefix_xor[i] ^ arr[i]
+
+    result = [prefix_xor[r + 1] ^ prefix_xor[l] for l, r in queries]
+    return result
