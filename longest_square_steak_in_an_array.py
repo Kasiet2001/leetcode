ca2005f9f8d42a703d317e5fe31653d4c5a4c1cc
Kasiet2001 <mursidinovakasiet@gmail.com>
2024-10-28 06:30:09 +0600

    new solved leetcode problem

--- longest_square_steak_in_an_array.py
+++ longest_square_steak_in_an_array.py
@@ -1,17 +1,14 @@
 def longestSquareStreak(nums):
-    ans = 1
-    nums = set(nums)
-    visited = set()
-    for i in sorted(nums):
-        if i in visited:
-            continue
-        visited.add(i)
-        c = 1
-        while i ** 2 in nums:
-            visited.add(i ** 2)
-            i *= i
-            c += 1
-        ans = max(c, ans)
-    return ans if ans > 1 else -1
+    ans = -1
+    squares = dict()
+    nums = sorted(set(nums), reverse=True)
+    for num in nums:
+        n = num * num
+        if n in squares and n != num:
+            squares[num] = squares[n] + 1
+            ans = max(ans, squares[num])
+        else:
+            squares[num] = 1
+    return ans
 print(longestSquareStreak([2,3,5,6,7]))
 
