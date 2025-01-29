19b6af4fcf872d03c6c3f5afab15568f45aaec26
Kasiet2001 <mursidinovakasiet@gmail.com>
2025-01-29 18:09:26 +0600

    new solved leetcode problem

---
+++ redundant_connection.py
@@ -0,0 +1,25 @@
+def findRedundantConnection(edges):
+    N = len(edges)
+    par = [i for i in range(N + 1)]
+    rank = [1] * (N + 1)
+
+    def find(n):
+        if n != par[n]:
+            par[n] = find(par[n])
+        return par[n]
+
+    def union(n1, n2):
+        p1, p2 = find(n1), find(n2)
+        if p1 == p2:
+            return False
+        if rank[p1] > rank[p2]:
+            par[p2] = p1
+            rank[p1] += rank[p2]
+        else:
+            par[p1] = p2
+            rank[p1] += rank[p2]
+        return True
+    for n1, n2 in edges:
+        if not union(n1, n2):
+            return [n1, n2]
+print(findRedundantConnection([[1,2],[2,3],[3,4],[1,4],[1,5]]))
