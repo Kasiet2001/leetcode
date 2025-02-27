889c758e7993415b60264b706363a4e3fff42cca
Kasiet2001 <mursidinovakasiet@gmail.com>
2025-02-27 06:21:11 +0600

    new solved leetcode problem

---
+++ length_of_longest_fibonacci_subsequence.py
@@ -0,0 +1,14 @@
+def lenLongestFibSubseq(arr):
+    num_set = set(arr)
+    max_len = 0
+    n = len(arr)
+    for start in range(n):
+        for next in range(start + 1, n):
+            prev = arr[next]
+            curr = arr[start] + arr[next]
+            curr_len = 2
+            while curr in num_set:
+                prev, curr = curr, curr + prev
+                curr_len += 1
+                max_len = max(curr_len, max_len)
+    return max_len
